458d22b64844ff57c39270f9bf043cba982bb1a8
Ozan Ay <ozanay@ku.edu.tr>
2018-07-07 14:13:25 +0300

    Initial commit

--- src/main/java/com/trade/bot/BotRunner.java
+++ src/main/java/com/trade/bot/BotRunner.java
@@ -16,5 +16,6 @@ public class BotRunner {
             TickerPrice btcUsdtPrice = client.getPrice("BTCUSDT");
             System.out.println("BTC/USDT prices: " + btcUsdtPrice.toString());
         }
+
     }
 }
